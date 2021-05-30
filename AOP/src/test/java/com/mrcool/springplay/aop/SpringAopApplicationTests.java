0e1c717e33975a275aa4bb390c29a66da64164e4
Mahendra Gadila <gadila.mahendra@gmail.com>
2021-05-30 14:14:08 +0530

    added checkstyle

---
+++ AOP/src/test/java/com/mrcool/springplay/aop/SpringAopApplicationTests.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2021-2021 MrCool.
+ *
+ */
+
+package com.mrcool.springplay.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * SpringPlay AOP demo application.
+ *
+ * @author Mahendra Gadila
+ * @since 1.0.0
+ */
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
