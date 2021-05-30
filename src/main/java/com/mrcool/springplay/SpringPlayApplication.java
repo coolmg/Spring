6d976e04b4bc6a65dca83d606c6bb7fa0c4ef85d
Mahendra Gadila <gadila.mahendra@gmail.com>
2021-05-30 11:14:17 +0530

    initial setup

---
+++ src/main/java/com/mrcool/springplay/SpringPlayApplication.java
@@ -0,0 +1,13 @@
+package com.mrcool.springplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrcool/springplay/SpringPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrcool.springplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
