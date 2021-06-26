4d1882f1519eb019aff22008882380ca49f91493
Mahendra Gadila <gadila.mahendra@gmail.com>
2021-06-26 19:20:43 +0530

    OAuth2 client

---
+++ OAuth2/src/main/java/com/example/SocialApplication.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2021-2021 MrCool.
+ *
+ */
+
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Class name.
+ *
+ * @author Mahendra Gadila.
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class SocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialApplication.class, args);
+	}
+
+}
