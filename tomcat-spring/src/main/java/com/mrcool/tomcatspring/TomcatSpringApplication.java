7a9c120a2fd4373082b3be357b1087cdbf8095f2
coolmg <gadila.mahendra@gmail.com>
2023-02-25 21:13:24 +0530

    tomcat spring

---
+++ tomcat-spring/src/main/java/com/mrcool/tomcatspring/TomcatSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mrcool.tomcatspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatSpringApplication.class, args);
+	}
+
+}
---
+++ tomcat-spring/src/test/java/com/mrcool/tomcatspring/TomcatSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrcool.tomcatspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
